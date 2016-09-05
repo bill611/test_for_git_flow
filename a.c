952aa15d5b95a0d9646982828f98ffc12c58ead1
bill611 <bill4regist@163.com>
2016-09-05 14:20:54 +0800

    add a.c, update main.c

---
+++ a.c
@@ -0,0 +1,6 @@
+#include "stdio.h"
+
+void test_a(void)
+{
+	printf("This a\n");
+}
--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include "stdio.h"
 
+extern void test_a(void);
+
 int main(void)
 {
 	printf("git test\n");	
+	test_a();
 	return 0;
 }
